d7957b0ec4f6b830b7cd6a44d1ecd2cb12bfc296
arian <arianpourinfo@gmail.com>
2015-06-28 15:37:06 +0800

    added the address fully function

---
+++ app/Http/Controllers/AddressController.php
@@ -0,0 +1,108 @@
+<?php namespace App\Http\Controllers;
+
+use App\Address;
+use App\Client;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Http\Requests\StoreAddressPostRequest;
+use App\Property;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class AddressController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+        return view('addAddress');
+	}
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StoreAddressPostRequest $request
+     * @return Response
+     */
+	public function store(StoreAddressPostRequest $request)
+	{
+
+        $address= new Address(array(
+            'unit'=> $request->unit,
+            'street'=>$request->street,
+            'postCode'=>$request->postcode,
+            'city'=>$request->city,
+            'state'=>$request->state,
+            'country'=>$request->country,
+        ));
+        switch(Session::get('AddRole')){
+            case "client":
+                $client= Client::find(Session::get('ClientInsertedId'));
+                $client->addresses()->save($address);
+            case "property":
+                $property=Property::find(Session::get('PropertyInsertedId'));
+                $property->addresses()->save($address);
+                break;
+        }
+
+        return redirect('rentalagreement/create');	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
---
+++ app/Http/Controllers/OwnerController.php
@@ -0,0 +1,104 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Http\Requests\StoreaddClientPostRequest;
+use App\Http\Requests\StoreaddClientPostRequestPostRequest;
+use App\Http\Requests\StoreAddOwnerPostRequest;
+use Illuminate\Http\Request;
+
+class OwnerController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+        //TODO: change it to show the owner lists
+        $clientList=User::findOrFail(Auth::user()->id)->client;
+        return view('clients',compact('clientList'));
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+        return view('addOwner');
+	}
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StoreAddClientPostRequest $request
+     * @return Response
+     */
+	public function store(StoreaddClientPostRequest $request)
+	{
+        $person = new Client(array(
+            'firstName'     =>  $request->firstName,
+            'lastName'      =>  $request->lastName,
+            'user_id'       =>  Auth::user()->id,
+            'nationality'   =>  $request->nationality,
+            'email'         =>  $request->email,
+            'idNumber'      =>  $request->idNumber,
+            'phoneNo'       =>  $request->phone,
+            'role'          =>  'owner'
+
+        ));
+        $person->save();
+        Session::put('ClientInsertedId', $person->id);
+        Session::put('AddRole', 'client');
+        return redirect('address/create');
+    }
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
